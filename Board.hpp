d5db1eb8f4739f7ff3825c8a1022fc6f14a17b98
Fa-zz <fansar8712@gmail.com>
2022-12-31 14:08:14 -0500

    Added basic funcitonality for creating a window

--- Board.hpp
+++ Board.hpp
@@ -2,5 +2,20 @@
 
 class Board {
 public:
+    // basic constructor
+    Board(int height, int width) {
+    int xMax, yMax;
+    getmaxyx(stdscr, yMax, xMax);
+
+    board_win = newwin(height, width, (yMax/2) - (height/2), (xMax/2)- (width/2));
+    box(board_win, 0, 0);
+    wrefresh(board_win);
+
+    this->height = height;
+    this->width = width;
+
+    }
 private:
+    WINDOW* board_win;
+    int height, width;
 };
